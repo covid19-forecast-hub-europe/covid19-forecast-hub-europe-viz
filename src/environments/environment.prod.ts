8ec75d57a55da4a0e0cb9b9690c915fa5d1bc033
Hugo Gruson <hugo.gruson@protonmail.com>
2021-09-28 15:08:39 +0200

    Get data files from git lfs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,16 +3,16 @@ export const environment = {
   base_href: '/forecast-europe',
   urls: {
     defaultSettings: {
-      modelNames: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/settings_model_selection.json'
+      modelNames: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/settings_model_selection.json'
     },
     lookups: {
-      location: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/location_codes.csv'
+      location: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/data-locations/locations_eu.csv'
     },
     forecastData: {
       csv: 'https://raw.githubusercontent.com/jbracher/covid19-forecast-hub-europe/main/viz/forecasts_to_plot.csv',
-      json: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/forecasts_to_plot.json'
+      json: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/forecasts_to_plot.json'
     },
-    truthData: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/truth_to_plot.csv',
-    metadata: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/metadata.json'
+    truthData: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/truth_to_plot.csv',
+    metadata: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/metadata.json'
   }
 };
