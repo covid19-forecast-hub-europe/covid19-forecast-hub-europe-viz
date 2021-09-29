2d7c2fcdacacfd0b32f2e3c191d23d009ea94791
Hugo Gruson <hugo.gruson@protonmail.com>
2021-09-29 12:38:56 +0200

    Get large files from azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,16 +3,16 @@ export const environment = {
   base_href: '/covid19-forecast-hub-europe-viz/',
   urls: {
     defaultSettings: {
-      modelNames: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/settings_model_selection.json'
+      modelNames: 'https://covid19forecasthubcdn.blob.core.windows.net/data/settings_model_selection.json'
     },
     lookups: {
       location: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/data-locations/locations_eu.csv'
     },
     forecastData: {
       csv: 'https://raw.githubusercontent.com/jbracher/covid19-forecast-hub-europe/main/viz/forecasts_to_plot.csv',
-      json: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/forecasts_to_plot.json'
+      json: 'https://covid19forecasthubcdn.blob.core.windows.net/data/forecasts_to_plot.json'
     },
-    truthData: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/truth_to_plot.csv',
-    metadata: 'https://media.githubusercontent.com/media/epiforecasts/covid19-forecast-hub-europe/main/viz/metadata.json'
+    truthData: 'https://covid19forecasthubcdn.blob.core.windows.net/data/truth_to_plot.csv',
+    metadata: 'https://covid19forecasthubcdn.blob.core.windows.net/data/metadata.json'
   }
 };
