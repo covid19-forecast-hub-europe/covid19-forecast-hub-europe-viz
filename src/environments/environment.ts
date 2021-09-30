2c2025805609ca9c398223f4a7734f2b5fc004b5
Hugo Gruson <hugo.gruson@protonmail.com>
2021-09-30 15:40:22 +0200

    Switch to akamai CDN to have compression for files > 8MB

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,17 @@ export const environment = {
   base_href: '',
   urls: {
     defaultSettings: {
-      modelNames: 'https://covid19forecasthubcdn.azureedge.net/data/settings_model_selection.json'
+      modelNames: 'https://covid19-forecasthub-cdn.azureedge.net/data/settings_model_selection.json'
     },
     lookups: {
       location: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/data-locations/locations_eu.csv'
     },
     forecastData: {
       csv: 'https://raw.githubusercontent.com/jbracher/covid19-forecast-hub-europe/main/viz/forecasts_to_plot.csv',
-      json: 'https://covid19forecasthubcdn.azureedge.net/data/forecasts_to_plot.json'
+      json: 'https://covid19-forecasthub-cdn.azureedge.net/data/forecasts_to_plot.json'
     },
-    truthData: 'https://covid19forecasthubcdn.azureedge.net/data/truth_to_plot.csv',
-    metadata: 'https://covid19forecasthubcdn.azureedge.net/data/metadata.json'
+    truthData: 'https://covid19-forecasthub-cdn.azureedge.net/data/truth_to_plot.csv',
+    metadata: 'https://covid19-forecasthub-cdn.azureedge.net/data/metadata.json'
   }
 };
 
