1e87c389cf36588a82e896f648a49f06be6522f4
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-12-09 14:54:20 +0000

    update download links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,15 +3,15 @@ export const environment = {
   // base_href: '/forecast-europe/',
   urls: {
     defaultSettings: {
-      modelNames: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/viz/settings_model_selection.json'
+      modelNames: 'https://covid19-forecasthub-eu-cdn-dkbuc4bverhgftfx.z01.azurefd.net/data/settings_model_selection.json'
     },
     lookups: {
-      location: 'https://raw.githubusercontent.com/epiforecasts/covid19-forecast-hub-europe/main/data-locations/locations_eu.csv'
+      location: 'https://covid19-forecasthub-eu-cdn-dkbuc4bverhgftfx.z01.azurefd.net/data/locations_eu.csv'
     },
     forecastData: {
-      json: 'https://covid19forecasthubecdc.blob.core.windows.net/data/forecasts_to_plot.json'
+      json: 'https://covid19-forecasthub-eu-cdn-dkbuc4bverhgftfx.z01.azurefd.net/data/forecasts_to_plot.json'
     },
-    truthData: 'https://covid19forecasthubecdc.blob.core.windows.net/data/truth_to_plot.csv',
-    metadata: 'https://covid19forecasthubecdc.blob.core.windows.net/data/metadata.json'
+    truthData: 'https://covid19-forecasthub-eu-cdn-dkbuc4bverhgftfx.z01.azurefd.net/data/truth_to_plot.csv',
+    metadata: 'https://covid19-forecasthub-eu-cdn-dkbuc4bverhgftfx.z01.azurefd.net/data/metadata.json'
   }
 };
